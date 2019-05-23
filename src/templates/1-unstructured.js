8dfd632f626876681e0e22284b015abae72b43c1
Jason Lengstorf <jason@lengstorf.com>
2019-05-23 13:12:18 -0700

    feat: add a hard-coded page template

---
+++ src/templates/1-unstructured.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Unstructured = () => (
+  <>
+    <h1>This is a hard-coded page</h1>
+    <p>All of the data is embedded directly into the React component.</p>
+  </>
+);
+
+export default Unstructured;
