4a41477be99748ddcfd2d26063f263c87b2c577b
Jason Lengstorf <jason@lengstorf.com>
2019-05-23 13:24:19 -0700

    feat: query for site metadata and use it in the page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,40 @@
-exports.createPages = ({ actions, reporter }) => {
+exports.createPages = async ({ actions, graphql, reporter }) => {
   reporter.info('Creating the unstructured page');
+
   actions.createPage({
     path: '/unstructured',
-    component: require.resolve('./src/templates/1-unstructured.js')
+    component: require.resolve('./src/templates/1-unstructured.js'),
+    context: {
+      title: 'This title came from context!',
+      body: 'The body copy, too!'
+    }
+  });
+
+  reporter.info('Create a page using site metadata');
+
+  const result = await graphql(`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panic('oh no', result.errors);
+  }
+
+  const { title, description } = result.data.site.siteMetadata;
+
+  actions.createPage({
+    path: '/meta',
+    component: require.resolve('./src/templates/1-unstructured.js'),
+    context: {
+      title,
+      body: description
+    }
   });
 };
