656ee6b4ab607be52f856a10cca3162c3d897c12
Jason Lengstorf <jason@lengstorf.com>
2019-05-24 17:36:18 -0700

    feat: add and use Gatsby plugin options

---
+++ lessons/03-plugin-options/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-meals',
+      options: {
+        diet: 'vegetarian',
+      },
+    },
+  ],
+};
---
+++ lessons/03-plugin-options/plugins/gatsby-plugin-meals/gatsby-node.js
@@ -0,0 +1,57 @@
+// Quick-and-dirty helper to convert strings into URL-friendly slugs.
+const slugify = str =>
+  str
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)+/g, '');
+
+const meals = [
+  {
+    key: 1,
+    name: 'Smoked Brisket',
+    diet: 'omnivore',
+  },
+  {
+    key: 2,
+    name: 'Ratatouille',
+    diet: 'vegetarian',
+  },
+  {
+    key: 3,
+    name: 'Chicken Pot Pie',
+    diet: 'omnivore',
+  },
+  {
+    key: 4,
+    name: 'Vegetable Fried Rice',
+    diet: 'vegetarian',
+  },
+];
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest, reporter },
+  pluginOptions,
+) => {
+  reporter.info(
+    `creating nodes for all meals that fit ${pluginOptions.diet} diets`,
+  );
+
+  const filteredMeals = meals.filter(meal => meal.diet === pluginOptions.diet);
+
+  filteredMeals.forEach(meal => {
+    actions.createNode({
+      // custom fields
+      ...meal,
+      slug: slugify(meal.name),
+
+      // required fields for Gatsby
+      id: createNodeId(`Meal-${meal.key}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Meal',
+        contentDigest: createContentDigest(meal),
+      },
+    });
+  });
+};
